b2415955cf5fd03fcbb75377b68e214aec3f9fd4
本間　康之 <creatures.access@gmail.com>
2022-01-07 00:07:17 +0900

    1.6 -6

--- imageapp/views.py
+++ imageapp/views.py
@@ -110,8 +110,8 @@ def signup(request):
 
 
 
-from django.views.decorators.csrf import requires_csrf_token
-from django.http import HttpResponseServerError
+# from django.views.decorators.csrf import requires_csrf_token
+# from django.http import HttpResponseServerError
 
 # @requires_csrf_token
 # def my_customized_server_error(request, template_name='500.html'):
