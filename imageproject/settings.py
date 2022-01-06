3dd2f06cdd33acbdd5c218a605530f1ec220302f
本間　康之 <creatures.access@gmail.com>
2022-01-06 22:44:07 +0900

    1.5 -5

--- imageproject/settings.py
+++ imageproject/settings.py
@@ -117,11 +117,25 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+
+
+# 追記
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # 追記
+# 追記
+
+
 STATIC_URL = '/static/'
 
 # import django_heroku
 # django_heroku.settings(locals())
+
+
+# 追記
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+# 追記
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
